86f9d0d95f50b9e0c8f5bd93cf21d645603d8600
Suraj Gopal <surajcancode@gmail.com>
2022-11-10 11:48:15 -0500

    unique movie_id & list_id combo solved

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,9 +2,5 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
   validates_length_of :comment, :minimum => 6
-  validate :check_movie_id_and_list_id
-
-  def check_movie_id_and_list_id
-    errors.add(:movie_id, "can't be the same as list id") if :movie_id == :list_id
-  end
+  validates_uniqueness_of :movie_id, { scope: :list_id }
 end
